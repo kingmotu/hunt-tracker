60dc53fcb72cd82a0da8cc1a3f31b9e938e2c441
kingmotu <kingmotu@gmx.de>
2022-11-09 09:56:08 +0100

    [INIT] inital vite project

---
+++ src/App.ts
@@ -0,0 +1,10 @@
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
