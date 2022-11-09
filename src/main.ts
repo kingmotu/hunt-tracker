33b11cc219845cc1f4dce0bdb9673b79aab30b8b
kingmotu <kingmotu@gmx.de>
2022-11-09 10:01:06 +0100

    [ADD] add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.use(createPinia())
-app.use(router)
+import "./assets/main.css";
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(vuetify);
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-card
+      width="400"
+      title="This is a title"
+      subtitle="This is a subtitle"
+      text="This is content"
+    ></v-card>
   </div>
 </template>
 
