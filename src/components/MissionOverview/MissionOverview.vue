824a2b3f6191d5a9f854f9f1d81899f005e2a848
kingmotu <kingmotu@gmx.de>
2022-11-30 10:53:23 +0100

    [CHANGE] add short kill overview

--- src/components/MissionOverview/MissionOverview.vue
+++ src/components/MissionOverview/MissionOverview.vue
@@ -3,11 +3,13 @@
     <v-tabs v-model="tab" bg-color="" centered>
       <v-tab value="tab-1"> Teams </v-tab>
 
-      <v-tab value="tab-2"> Mission-Log </v-tab>
+      <v-tab value="tab-2"> Kills </v-tab>
 
-      <v-tab value="tab-3"> Events </v-tab>
+      <v-tab value="tab-3"> Mission-Log </v-tab>
 
-      <v-tab value="tab-4"> Summary </v-tab>
+      <v-tab value="tab-4"> Events </v-tab>
+
+      <v-tab value="tab-5"> Summary </v-tab>
     </v-tabs>
 
     <v-window v-model="tab">
@@ -26,15 +28,44 @@
       </v-window-item>
       <v-window-item value="tab-2">
         <v-container>
-          <MissionLog :mission-log="missionData.missionLog" />
+          <v-row>
+            <v-col cols="2"></v-col>
+            <v-col cols="1">Team</v-col>
+            <v-col cols="1">Self</v-col>
+            <v-col cols="1">Assists</v-col>
+            <v-col cols="1">Summ</v-col>
+            <v-col cols="6"></v-col>
+
+            <v-col cols="2">Kills </v-col>
+            <v-col cols="1">{{ missionData.missionKills.teamKills }}</v-col>
+            <v-col cols="1">{{ missionData.missionKills.ownKills }}</v-col>
+            <v-col cols="1">{{ missionData.missionKills.assists || 0 }}</v-col>
+            <v-col cols="1">{{
+              missionData.missionKills.teamKills + missionData.missionKills.ownKills
+            }}</v-col>
+            <v-col cols="6"></v-col>
+
+            <v-col cols="2">Deaths </v-col>
+            <v-col cols="1">{{ missionData.missionKills.teamDeaths }}</v-col>
+            <v-col cols="1">{{ missionData.missionKills.ownDeaths }}</v-col>
+            <v-col cols="1"></v-col>
+            <v-col cols="1">{{
+              missionData.missionKills.teamDeaths + missionData.missionKills.ownDeaths
+            }}</v-col>
+          </v-row>
         </v-container>
       </v-window-item>
       <v-window-item value="tab-3">
         <v-container>
-          <MissionEvents :mission-entries="missionData.Entries" />
+          <MissionLog :mission-log="missionData.missionLog" />
         </v-container>
       </v-window-item>
       <v-window-item value="tab-4">
+        <v-container>
+          <MissionEvents :mission-entries="missionData.Entries" />
+        </v-container>
+      </v-window-item>
+      <v-window-item value="tab-5">
         <v-container>
           <MissionSummary :mission-accolades="missionData.Accolades" />
         </v-container>
